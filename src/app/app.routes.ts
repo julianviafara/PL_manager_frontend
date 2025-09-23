a791d8d2ab1a9b55a075cc6fcafb402c1007e104
Juli <viafarajulian10@gmail.com>
2025-09-23 17:19:50 -0500

    Fix git error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MusicosGrupoComponent } from './musicos-grupo/musicos-grupo.component';
+import { ProductosComponent } from './productos/productos.component';
+import { DeudasComponent } from './deudas/deudas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'musicos', component: MusicosGrupoComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'deudas', component: DeudasComponent },
+  { path: '', redirectTo: '/musicos', pathMatch: 'full' },
+  { path: '**', redirectTo: '/musicos' }
+];
